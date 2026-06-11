5e8d3734308c869421959abc95527c4451424d32
Eduard <eduarmurcia8@gmail.com>
2026-06-11 15:46:19 -0500

    Fix: db.php now loads credentials from private folder outside public_html

---
+++ config/db.php
@@ -0,0 +1,4 @@
+<?php
+// Las credenciales viven fuera del directorio público para no ser borradas por git deploy
+// Archivo real: /home/u294080658/private/db.php
+require_once '/home/u294080658/private/db.php';
